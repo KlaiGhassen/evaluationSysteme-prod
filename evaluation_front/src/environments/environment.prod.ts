68fcf48c6fd55fea544eb8c2a8f816794dfbbff5
Klai Ghassen <60801440+KlaiGhassen@users.noreply.github.com>
2024-02-04 17:52:23 +0100

    test

--- evaluation_front/src/environments/environment.prod.ts
+++ evaluation_front/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:3040/api/',
+    apiUrl: 'http://espritmobile.ovh/api/',
     issuer: 'https://accounts.google.com',
     clientId:
         '830033697803-07hrq9qctj2ba61ugra0i84cl6lsdabj.apps.googleusercontent.com',
