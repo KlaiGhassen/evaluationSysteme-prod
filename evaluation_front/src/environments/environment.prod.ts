5432c13b442b890beccbe7b43b4cfd72d2f1a8f3
KlaiGhassen <ghassen.klai@esprit.tn>
2025-06-14 02:52:39 +0100

    last changes

--- evaluation_front/src/environments/environment.prod.ts
+++ evaluation_front/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://espritmobile.com/api/',
+    apiUrl: 'https://espritmobile.com/api',
     issuer: 'https://accounts.google.com',
     clientId:
         '830033697803-07hrq9qctj2ba61ugra0i84cl6lsdabj.apps.googleusercontent.com',
